c907a2e1802a00547d10579aec2db8b0d30b6e83
regice42 <0x359281@proton.me>
2025-04-30 13:35:17 +0300

    Add BUSL-1.1 license

--- contracts/libraries/helpers/Decimals.sol
+++ contracts/libraries/helpers/Decimals.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
